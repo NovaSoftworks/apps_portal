b43b0793b473beb69412c3694a126b53e77b1901
marclamy <marc.lamy83@gmail.com>
2023-12-29 04:41:00 +0000

    initialize the SvelteKit app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div
+	class="container"
+	style="display: flex; justify-content: center; align-items: center; height: 100vh; font-family:Arial, Helvetica, sans-serif;"
+>
+	WIP - Nova Softworks
+</div>

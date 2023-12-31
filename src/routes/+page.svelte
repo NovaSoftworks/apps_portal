a8c8d877b260884cd091968c803992f569479d0c
marclamy <marc.lamy83@gmail.com>
2023-12-31 20:58:05 +0000

    create the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,36 @@
-<div
-	class="container"
-	style="display: flex; justify-content: center; align-items: center; height: 100vh; font-family:Arial, Helvetica, sans-serif;"
->
-	WIP - Nova Softworks
+<div class="landing-page__container">
+	<h1>Epics <span class="primary">ahead</span></h1>
+	<h2>We don't just build software; we craft legacies.</h2>
 </div>
+
+<style>
+	h1 {
+		margin: 0;
+		padding: 0;
+
+		font-size: 4rem;
+		font-family: Montserrat, sans-serif;
+		font-weight: normal;
+		text-align: center;
+	}
+
+	h2 {
+		margin: 0;
+		padding: 0;
+
+		font-size: 1.25rem;
+		font-family: Oswald, sans-serif;
+		font-weight: normal;
+		text-transform: uppercase;
+		text-align: center;
+	}
+
+	.landing-page__container {
+		display: flex;
+		flex-direction: column; /* Aligns the children (title and subtitle) in a column */
+		justify-content: center; /* Vertically centers the flex children within the center block */
+		align-items: center; /* Horizontally centers the flex children within the center block */
+		height: 100%; /* Uses the full height of the viewport */
+		width: 100%; /* Uses the full width of the container */
+	}
+</style>
